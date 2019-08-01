62e9a45116afd903c7c9f157c4422df3db85c08f
Daniel1126 <www.houqi19921020@qq.com>
2019-08-01 17:18:41 +0800

    添加多线程爬取数据的调用实例，用于同时抓取多支股票的数据，推荐使用代理爬取

--- xueqiu_data_crawler/main_func_test.py
+++ xueqiu_data_crawler/main_func_test.py
@@ -5,10 +5,14 @@ Created on Mon Jul 22 10:43:12 2019
 @author: Daniel
 """
 
-from data import TechData
+from data import TechData,MultiTechData
+
 
 if __name__ == '__main__':
     '''
-    调用实例，如果proxy为False则不需要添加update参数，如果proxy为True则必须添加update参数
+    调用实例
     '''
+    # 单独调用数据
     df = TechData(symbol='00700',begin='20200801',period='day',type_='before',count=1000,indicator='').request(proxy=True,update=False) 
+    # 多线程调用数据
+    df = MultiTechData(symbols=['00700','SZ000651','AAPL','SZ002507','SH601318'],begin='20190719',period='day',type_='before',count=1000,indicator='',update=False).multi_request(proxy=True)
