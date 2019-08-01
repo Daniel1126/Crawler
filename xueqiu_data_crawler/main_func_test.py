ab7e6dda767948e103b058b66e6247d603e5c038
Daniel1126 <www.houqi19921020@qq.com>
2019-08-01 10:50:52 +0800

    添加代理调用实例

--- xueqiu_data_crawler/main_func_test.py
+++ xueqiu_data_crawler/main_func_test.py
@@ -4,10 +4,11 @@ Created on Mon Jul 22 10:43:12 2019
 
 @author: Daniel
 """
+
 from data import TechData
 
 if __name__ == '__main__':
     '''
-    调用实例
+    调用实例，如果proxy为False则不需要添加update参数，如果proxy为True则必须添加update参数
     '''
-    df = TechData(symbol='BABA',begin='20190719',period='day',type_='before',count=1000,indicator='').request()   
+    df = TechData(symbol='00700',begin='20200801',period='day',type_='before',count=1000,indicator='').request(proxy=True,update=False) 
