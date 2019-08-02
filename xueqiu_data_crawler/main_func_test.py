a15bb9ed622146da1bb875cf24b7550cfe274f57
Daniel1126 <www.houqi19921020@qq.com>
2019-08-02 12:38:30 +0800

    添加了CSI300，A50，中证500数据调用实例，及其推荐调用指数数据时proxy参数为True

--- xueqiu_data_crawler/main_func_test.py
+++ xueqiu_data_crawler/main_func_test.py
@@ -5,7 +5,7 @@ Created on Mon Jul 22 10:43:12 2019
 @author: Daniel
 """
 
-from data import TechData,MultiTechData
+from data import TechData,MultiTechData,IndexData
 
 
 if __name__ == '__main__':
@@ -13,6 +13,10 @@ if __name__ == '__main__':
     调用实例
     '''
     # 单独调用数据
-    df = TechData(symbol='00700',begin='20200801',period='day',type_='before',count=1000,indicator='').request(proxy=True,update=False) 
+    df_single = TechData(symbol='00700',begin='20200801',period='day',type_='before',count=1000,indicator='').request(proxy=True,update=False) 
     # 多线程调用数据
-    df = MultiTechData(symbols=['00700','SZ000651','AAPL','SZ002507','SH601318'],begin='20190719',period='day',type_='before',count=1000,indicator='',update=False).multi_request(proxy=True)
+    df_multi = MultiTechData(symbols=['00700','SZ000651','AAPL','SZ002507','SH601318'],begin='20190719',period='day',type_='before',count=1000,indicator='',update=False).multi_request(proxy=True)
+    # 调用沪深300/上证50/中证500个股数据，由于同时获取多只股票数据，因此速度较慢
+    df_CSI300 = IndexData(begin='20210719',period='day',type_='before',count=1000,indicator='',update=False).index_request(index='csi300',proxy=True)
+    df_SH50   = IndexData(begin='20210719',period='day',type_='before',count=1000,indicator='',update=False).index_request(index='sh50',proxy=True)
+    df_CSI500 = IndexData(begin='20210719',period='day',type_='before',count=1000,indicator='',update=False).index_request(index='csi500',proxy=True)
